500d1d1479d6cff14b83aa93cf31f9d5469cd3e6
silviafuda <inzifusa.s@gmail.com>
2013-06-14 18:21:00 +0200

    terza prova commit

---
+++ db/migrate/20130529103732_create_projects.rb
@@ -0,0 +1,12 @@
+class CreateProjects < ActiveRecord::Migration
+  def change
+    create_table :projects do |t|
+      t.string :content
+      t.integer :user_id
+
+      t.timestamps
+    end
+    # index to help us retrieve all the projects associated with a given user in reverse order
+    add_index :projects, [:user_id, :created_at]
+  end
+end

f08a889f04cd96460c048f5028d68e682ebfc475
gionnyb <giovialeb@gmail.com>
2013-06-15 11:16:44 +0200

    modificato titolo pagina

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Return a title on a per-page basis
   def full_title(page_title)
-    base_title = 'SWorD'
+    base_title = 'CrowdFooding'
     if page_title.empty?
       base_title
     else

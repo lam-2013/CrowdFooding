676696d9629e6681cec95fa4b81879951e1d2bc0
silviafuda <inzifusa.s@gmail.com>
2013-06-26 16:24:44 +0200

    traduzione dei Progetti terminati con successo e correzione sql

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -106,7 +106,7 @@ class ProjectsController < ApplicationController
 
   def findProjects_complete
 
-    Project.find_by_sql("SELECT * FROM projects WHERE data_fine < current_timestamp AND budget_attuale > goal ORDER BY budget_attuale DESC")
+    Project.find_by_sql("SELECT * FROM projects WHERE data_fine < current_timestamp AND budget_attuale > goal ORDER BY data_fine DESC")
 
   end
 
